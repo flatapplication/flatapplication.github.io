8637812621c2470acfb75045ed4c798156a24563
Talha A <42ahmad@gmail.com>
2018-09-15 16:29:47 -0400

    quick change of class -> className for react

--- sidebar.js
+++ sidebar.js
@@ -1,36 +1,36 @@
-class Sidebar extends React.Component{
-    render(){
+class Sidebar extends React.Component {
+    render() {
         return (
             <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
-                    <a href="#">
-                        F L A T
+                <ul className="sidebar-nav">
+                    <li className="sidebar-brand">
+                        <a href="#">
+                            F L A T
                     </a>
-                </li>
-                <li style="font-size:17px;">
-                    <a href="yourroom.html">Your Room</a>
-                </li>
-                <li style="font-size:17px;">
-                    <a href="youritems.html">Items You Own</a>
-                </li>
-                <li style="font-size:17px;">
-                    <a href="loaneditems.html">Items Loaned Out</a>
-                </li>
-                <li style="font-size:17px;">
-                    <a href="requesteditems.html">Items Requested</a>
-                </li>
-                <li style="font-size:17px;">
-                    <a href="pastitems.html">Past Items</a>
-                </li>
-                <li style="font-size:17px;">
-                    <a href="nearby.html">Find Item Nearby</a>
-                </li>
-                <li style="font-size:17px;">
-                    <a href="support.html">Support</a>
-                </li>
-            </ul>
-        </div>
+                    </li>
+                    <li style="font-size:17px;">
+                        <a href="yourroom.html">Your Room</a>
+                    </li>
+                    <li style="font-size:17px;">
+                        <a href="youritems.html">Items You Own</a>
+                    </li>
+                    <li style="font-size:17px;">
+                        <a href="loaneditems.html">Items Loaned Out</a>
+                    </li>
+                    <li style="font-size:17px;">
+                        <a href="requesteditems.html">Items Requested</a>
+                    </li>
+                    <li style="font-size:17px;">
+                        <a href="pastitems.html">Past Items</a>
+                    </li>
+                    <li style="font-size:17px;">
+                        <a href="nearby.html">Find Item Nearby</a>
+                    </li>
+                    <li style="font-size:17px;">
+                        <a href="support.html">Support</a>
+                    </li>
+                </ul>
+            </div>
         )
     }
 }
