69d3cf6826a1ea3701ebbb9cac920c741b8c5a4e
spritefullake <42ahmad@gmail.com>
2018-09-15 16:37:30 -0400

    update styling attributes to conform to react

--- sidebar.js
+++ sidebar.js
@@ -8,25 +8,25 @@ class Sidebar extends React.Component {
                             F L A T
                     </a>
                     </li>
-                    <li style="font-size:17px;">
+                    <li style={{fontSize:17}}>
                         <a href="yourroom.html">Your Room</a>
                     </li>
-                    <li style="font-size:17px;">
+                    <li style={{fontSize:17}}>
                         <a href="youritems.html">Items You Own</a>
                     </li>
-                    <li style="font-size:17px;">
+                    <li style={{fontSize:17}}>
                         <a href="loaneditems.html">Items Loaned Out</a>
                     </li>
-                    <li style="font-size:17px;">
+                    <li style={{fontSize:17}}>
                         <a href="requesteditems.html">Items Requested</a>
                     </li>
-                    <li style="font-size:17px;">
+                    <li style={{fontSize:17}}>
                         <a href="pastitems.html">Past Items</a>
                     </li>
-                    <li style="font-size:17px;">
+                    <li style={{fontSize:17}}>
                         <a href="nearby.html">Find Item Nearby</a>
                     </li>
-                    <li style="font-size:17px;">
+                    <li style={{fontSize:17}}>
                         <a href="support.html">Support</a>
                     </li>
                 </ul>
