fd63add7eac3626adcfe129254cc4be6d89e75eb
Kedar Abhyankar <kedz07@gmail.com>
2018-09-15 18:08:35 -0400

    changed sidebar

---
+++ nonLoggedInSidebar.js
@@ -0,0 +1,22 @@
+class nonLoggedInSidebar extends React.Component {
+    render() {
+        return (
+            <div id="sidebar-wrapper">
+                <ul className="sidebar-nav">
+                    <li className="sidebar-brand">
+                        <a href="#">
+                            F L A T
+                    </a>
+                    </li>
+                    <li style={{fontSize:17}}>
+                        <a href="support.html">Support</a>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+const e = React.createElement;
+const domContainer = document.querySelector('#sidebar');
+ReactDOM.render(e(Sidebar), domContainer);
